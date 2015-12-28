e7ef3a6a710c89dedff96b8e0ea2cebf6cbbd1a6
Jonathan Darling <jmdarling@me.com>
2015-12-28 17:03:51 -0600

    Adds logging. Enables CORS.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express')
 var app = express()
 
+app.use(function (request, response, next) {
+  // Log request
+  console.log(request.method, 'request to', request.path)
+
+  // Enable cors
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
+  // Move on to next middleware
+  next()
+})
+
 app.get('/', function (request, response) {
   response.send('swag')
 })
 
-app.listen(8080)
+var server = app.listen(3000, function () {
+  console.log('Listening on port', server.address().port)
+})
